758c2dae9f08e68db089cab113163c48a2d19a16
BuildTools <unconfigured@null.spigotmc.org>
2024-01-15 00:28:40 +0900

    2.0

--- src/main/java/prj/salmon/clickminecart/ClickMinecart.java
+++ src/main/java/prj/salmon/clickminecart/ClickMinecart.java
@@ -1,8 +1,10 @@
 package prj.salmon.clickminecart;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -10,6 +12,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.block.Action;
 
 public final class ClickMinecart extends JavaPlugin implements Listener {
 
@@ -20,13 +23,17 @@ public final class ClickMinecart extends JavaPlugin implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onPlayerInteractEvent(PlayerInteractEvent event){
         Location loc = event.getClickedBlock().getLocation();
-        if(loc.getBlock().getType().equals(Material.RAIL)){
-            World world = org.bukkit.Bukkit.getServer().getWorld("world");
+        Action action = event.getAction();
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+        if (action.equals(Action.LEFT_CLICK_BLOCK)) {
+            if (block.getType().equals(Material.RAIL)) {
+            World world = Bukkit.getServer().getWorld("world");
             Entity minecart = world.spawnEntity(loc, EntityType.MINECART);
-            Player player = event.getPlayer();
             minecart.addPassenger(player);
         }
     }
 
 
+}
 }
